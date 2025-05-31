e844de77f5ce73512f90631e9e67caf82b37b536
Riley Flynn <riley@rileyflynn.me>
2025-05-31 13:50:17 -0230

    Shut down on ctrl-c

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,20 @@ const app = new Hono();
 
 app.get("/", (c) => c.html(<Home />));
 
-serve(app, (info) => {
+const server = serve(app, (info) => {
 	console.log(`Listening on http://localhost:${info.port}`);
+});
+
+process.on("SIGINT", () => {
+	server.close();
+	process.exit(0);
+});
+process.on("SIGTERM", () => {
+	server.close((err) => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		process.exit(0);
+	});
 });
